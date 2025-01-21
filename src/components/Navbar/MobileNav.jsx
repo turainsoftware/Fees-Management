4bbb76f3f7643b209f85b3a22f2ebf8ae526f7a5
jhantu626 <jhantubala626@gmail.com>
2025-01-21 18:25:01 +0530

    added the tab bar image conditionally

--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -1,51 +1,124 @@
+// import React from 'react';
+// import menuActive from './../../assets/images/dashboard/menu-active.svg';
+// import menuNotActive from './../../assets/images/dashboard/menu-not-active.svg';
+// import { Link, NavLink } from 'react-router-dom';
+
+// const MobileNav = () => {
+//   return (
+//     <div className="bottomMenu bg-white border-top border-subtile-secondary">
+//       <div className="menuBTM px-14">
+//         <ul className="d-flex justify-content-evenly flex-wrap m-0 align-items-center">
+//           <li className="text-center">
+//             <NavLink
+//               to={'dashboard'}
+//               className="active d-flex align-items-center flex-column justify-content-around text-center position-relative"
+//             >
+//               <i className="ri-home-5-line"></i>
+//               <span>Home</span>
+//               <img src={menuActive} alt="Active Menu" />
+//             </NavLink>
+//           </li>
+//           <li className="text-center">
+//             <NavLink to={'students'}
+//               className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
+//             >
+//               <i className="ri-user-search-line"></i>
+//               <span>Student</span>
+//               <img src={menuNotActive} alt="Not Active Menu" />
+//             </NavLink>
+//           </li>
+//           <li className="text-center">
+//             <NavLink
+//               href="#"
+//               className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
+//             >
+//               <i className="ri-wallet-2-line"></i>
+//               <span>Fees</span>
+//               <img src={menuNotActive} alt="Not Active Menu" />
+//             </NavLink>
+//           </li>
+//           <li className="text-center">
+//             <NavLink
+//               href="#"
+//               className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
+//             >
+//               <i className="ri-menu-line"></i>
+//               <span>Menu</span>
+//               <img src={menuNotActive} alt="Not Active Menu" />
+//             </NavLink>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MobileNav;
+
+
+
 import React from 'react';
 import menuActive from './../../assets/images/dashboard/menu-active.svg';
 import menuNotActive from './../../assets/images/dashboard/menu-not-active.svg';
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const MobileNav = () => {
+    const location = useLocation();
+
   return (
     <div className="bottomMenu bg-white border-top border-subtile-secondary">
       <div className="menuBTM px-14">
         <ul className="d-flex justify-content-evenly flex-wrap m-0 align-items-center">
           <li className="text-center">
-            <Link
+            <NavLink
               to={'dashboard'}
-              className="active d-flex align-items-center flex-column justify-content-around text-center position-relative"
+              className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
             >
               <i className="ri-home-5-line"></i>
               <span>Home</span>
-              <img src={menuActive} alt="Active Menu" />
-            </Link>
+             <img
+                  src={location.pathname.includes('dashboard') ? menuActive : menuNotActive}
+                  alt={location.pathname.includes('dashboard') ? "Active Menu" : "Not Active Menu"}
+              />
+            </NavLink>
           </li>
           <li className="text-center">
-            <Link to={'students'}
+            <NavLink to={'students'}
               className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
             >
               <i className="ri-user-search-line"></i>
               <span>Student</span>
-              <img src={menuNotActive} alt="Not Active Menu" />
-            </Link>
+               <img
+                  src={location.pathname.includes('students') ? menuActive : menuNotActive}
+                  alt={location.pathname.includes('students') ? "Active Menu" : "Not Active Menu"}
+                />
+            </NavLink>
           </li>
           <li className="text-center">
-            <a
-              href="#"
+            <NavLink
+              to={'fees'}
               className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
             >
               <i className="ri-wallet-2-line"></i>
               <span>Fees</span>
-              <img src={menuNotActive} alt="Not Active Menu" />
-            </a>
+               <img
+                  src={location.pathname.includes('fees') ? menuActive : menuNotActive}
+                  alt={location.pathname.includes('fees') ? "Active Menu" : "Not Active Menu"}
+                />
+            </NavLink>
           </li>
           <li className="text-center">
-            <a
-              href="#"
+            <NavLink
+                to={'menu'}
               className="d-flex align-items-center flex-column justify-content-around text-center position-relative"
             >
               <i className="ri-menu-line"></i>
               <span>Menu</span>
-              <img src={menuNotActive} alt="Not Active Menu" />
-            </a>
+              <img
+                  src={location.pathname.includes('menu') ? menuActive : menuNotActive}
+                  alt={location.pathname.includes('menu') ? "Active Menu" : "Not Active Menu"}
+              />
+            </NavLink>
           </li>
         </ul>
       </div>
